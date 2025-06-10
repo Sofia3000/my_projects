6ebb8439283c4def86592e94cca0fafeca334197
Sofia3000 <almira20@ukr.net>
2025-06-10 15:16:50 +0300

    Delete all projects

--- commerce/auctions/utils.py
+++
@@ -1,6 +0,0 @@
-from .models import Category
-def get_categories():
-    categories = Category.objects.all().values('id', 'name')
-    categories_list = [(c['id'], c['name']) for c in categories]
-    categories_list.insert(0, (None, "No category"))
-    return tuple(categories_list)
